b8e0c42c7b68d96595b500f6e80a709c00d09c07
Josh <wjang717@gmail.com>
2023-02-16 13:26:20 -0800

    added status code

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,6 +3,7 @@ const models = require('../models');
 module.exports = {
   sayHello: (req, res) => {
     const something = models.sayHello();
+    res.sendStatus(200);
     res.send(something);
   },
 };
