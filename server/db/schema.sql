bce7ac2132d1d2e9114750c7cdd98cfd4e3db51f
Josh <wjang717@gmail.com>
2023-02-24 20:06:03 -0800

    finished setting up routes

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -4,17 +4,17 @@ CREATE DATABASE ratingsreviews;
 \c ratingsreviews;
 
 CREATE TABLE reviews (
-  id INTEGER PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   product_id INTEGER,
   rating INTEGER,
   date TEXT,
   summary TEXT,
   body TEXT,
   recommend BOOLEAN,
-  reported BOOLEAN,
+  reported BOOLEAN DEFAULT false,
   name VARCHAR(50),
   email VARCHAR(100),
-  response TEXT,
+  response TEXT DEFAULT NULL,
   helpfulness INTEGER DEFAULT 0
 );
 
@@ -22,8 +22,10 @@ COPY reviews
   FROM '/Users/wooseokjang/Desktop/SDC/review_service/ETL/csv/reviews.csv'
   DELIMITER ',' NULL AS 'null' CSV HEADER;
 
+ALTER SEQUENCE reviews_id_seq RESTART WITH 5774953;
+
 CREATE TABLE photos (
-  id INTEGER PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   review_id INTEGER REFERENCES reviews (id) NOT NULL,
   url TEXT
 );
@@ -32,8 +34,10 @@ COPY photos
   FROM '/Users/wooseokjang/Desktop/SDC/review_service/ETL/csv/reviews_photos.csv'
   DELIMITER ',' NULL AS 'null' CSV HEADER;
 
+ALTER SEQUENCE photos_id_seq RESTART WITH 2742541;
+
 CREATE TABLE characteristics (
-  id INTEGER PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   product_id INTEGER NOT NULL,
   name VARCHAR(50)
 );
@@ -42,8 +46,10 @@ COPY characteristics
   FROM '/Users/wooseokjang/Desktop/SDC/review_service/ETL/csv/characteristics.csv'
   DELIMITER ',' NULL AS 'null' CSV HEADER;
 
+ALTER SEQUENCE characteristics_id_seq RESTART WITH 3347680;
+
 CREATE TABLE characteristics_reviews (
-  id INTEGER PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   characteristics_id INTEGER REFERENCES characteristics (id) NOT NULL,
   review_id INTEGER REFERENCES reviews (id) NOT NULL,
   value INTEGER
@@ -52,3 +58,5 @@ CREATE TABLE characteristics_reviews (
 COPY characteristics_reviews
   FROM '/Users/wooseokjang/Desktop/SDC/review_service/ETL/csv/characteristic_reviews.csv'
   DELIMITER ',' NULL AS 'null' CSV HEADER;
+
+ALTER SEQUENCE characteristics_reviews_id_seq RESTART WITH 19327576;
--- server/models/index.js
+++ server/models/index.js
@@ -1,22 +1,92 @@
+const Promise = require('bluebird');
 const db = require('../db');
 
 module.exports = {
   getReviewsByProduct: (productId, page, count, sort, cb) => {
-    cb(null, 'here are your reviews');
-    // query database with given input
-    //  if err callback with error
-    //  if no error callback without error
+    db.query(`SELECT * FROM reviews WHERE product_id = ${productId};`)
+      .then((res) => {
+        const result = res.rows.map((review) => db.query(`SELECT id, url FROM photos WHERE review_id = ${review.id}`)
+          .then((photos) => ({ ...review, photos: photos.rows })));
+        Promise.all(result).then((value) => cb(null, value));
+      });
   },
   getMetaData: (productId, cb) => {
-    cb(null, `here is your metadata for ${productId}`);
+    // cb(null);
+    const result = {
+      product_id: productId,
+      ratings: {},
+      recommended: { false: 0, true: 0 },
+      characteristics: {},
+    };
+    const characteristics = {};
+    db.query(`SELECT id,rating,recommend FROM reviews WHERE product_id = ${productId}`)
+      .then((data) => {
+        // console.log(data.rows);
+        data.rows.forEach((row) => {
+          if (result.ratings[row.rating] === undefined) {
+            result.ratings[row.rating] = 1;
+          } else {
+            result.ratings[row.rating] += 1;
+          }
+          result.recommended[row.recommend] += 1;
+        });
+        const mapped = data.rows.map((row) => db.query(`SELECT characteristics_id, value FROM characteristics_reviews WHERE review_id = ${row.id}`));
+        Promise.all(mapped).then((res) => {
+          res.forEach((review) => {
+            review.rows.forEach((row) => {
+              if (characteristics[row.characteristics_id] === undefined) {
+                characteristics[row.characteristics_id] = row.value;
+              } else {
+                characteristics[row.characteristics_id] += row.value;
+              }
+            });
+          });
+          const keys = Object.keys(characteristics);
+          const map = keys.map((key) => db.query(`SELECT name FROM characteristics WHERE id = ${key}`)
+            .then((name) => {
+              const value = characteristics[key] / res.length;
+              result.characteristics[name.rows[0].name] = { id: key, value };
+            }));
+          Promise.all(map).then(() => cb(null, result));
+        });
+      });
   },
-  addReview: (data, cb) => {
-    cb(null);
+  addReview: async (data, cb) => {
+    const {
+      product_id, rating, summary, body, recommend, name, email, photos, characteristics,
+    } = data;
+    const text1 = 'INSERT INTO reviews (product_id, rating, date, summary, body, recommend, name, email) VALUES($1,$2,$3,$4,$5,$6,$7,$8)';
+    const values1 = [product_id, rating, '10000', summary, body, recommend, name, email];
+    await db.query(text1, values1);
+    const text2 = 'SELECT id FROM reviews WHERE product_id = $1 AND summary = $2';
+    const values2 = [product_id, summary];
+    db.query(text2, values2)
+      .then((review) => {
+        photos.forEach(async (photo) => {
+          const text3 = 'INSERT INTO photos (review_id, url) VALUES($1,$2)';
+          const values3 = [review.rows[0].id, photo];
+          await db.query(text3, values3);
+        });
+        Object.keys(characteristics).forEach(async (characteristic) => {
+          const text4 = 'INSERT INTO characteristics_reviews (characteristics_id, review_id, value) VALUES ($1,$2,$3)';
+          const values4 = [characteristic, review.rows[0].id, characteristics[characteristic]];
+          await db.query(text4, values4);
+        });
+        cb(null);
+      });
   },
-  setHelpfulReview: (productId, cb) => {
-    cb(null);
+  setHelpfulReview: (reviewId, cb) => {
+    db.query(`SELECT helpfulness FROM reviews WHERE id =${reviewId}`)
+      .then((help) => {
+        db.query(`UPDATE reviews SET helpfulness = ${help.rows[0].helpfulness + 1} WHERE id =${reviewId}`)
+          .then(() => { cb(null); });
+      });
   },
-  reportReview: (productId, cb) => {
-    cb(null);
+  reportReview: (reviewId, cb) => {
+    db.query(`SELECT reported FROM reviews WHERE id =${reviewId}`)
+      .then((report) => {
+        db.query(`UPDATE reviews SET reported = ${!report.rows[0].reported} WHERE id = ${reviewId}`)
+          .then(cb(null));
+      });
   },
 };
